0678bed2aafe1b635fe53a73a03cd608363b5234
alanpereiradasilva <161897310+alanpereiradasilva@users.noreply.github.com>
2026-02-25 15:58:51 -0300

    Aprendendo condições em JS

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é de ${vel}Km/h.`)
+// Condição em JS
+if (vel > 60) { // Condição simples!
+    console.log(`Você ultrapassou a velocidade permitida, MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
